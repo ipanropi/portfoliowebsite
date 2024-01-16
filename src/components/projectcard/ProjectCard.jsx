90ddc8bfb3de8f2a85c4c942f3f578500e5b6083
ipanropi <irfan100053@gmail.com>
2024-01-16 12:58:00 -0600

    Changes css styles to make the website more responsive and follow the common styles recommended

--- src/components/projectcard/ProjectCard.jsx
+++ src/components/projectcard/ProjectCard.jsx
@@ -13,16 +13,14 @@ const ProjectCard = ({data, text, isPreview}) => {
 
 
     const setSearch = (search) => {
-        if (search){
+        if (search) {
             newData = data.filter((project) =>
                 project.name.toLowerCase().includes(search.toLowerCase())
                 || project.description.toLowerCase().includes(search.toLowerCase())
                 || project.tech.some((tech) => tech.toLowerCase().includes(search.toLowerCase())))
             console.log(newData);
             setFilteredData(newData)
-        }
-
-        else {
+        } else {
             setFilteredData(data)
         }
     }
@@ -37,26 +35,28 @@ const ProjectCard = ({data, text, isPreview}) => {
             </div>
             <div className={styles.cardContainer}>
                 {filteredData.length > 0 ? (filteredData.map((project) => {
-                    return (
+                        return (
                             <div key={project.name} className={styles.card}>
                                 <Link className={styles.links} href={`/project/${project._id}`} key={project._id}>
-                                <div className={styles.imageContainer}>
-                                    <Image
-                                        className={styles.image}
-                                        src={project.image}
-                                        alt={project.name}
-                                        width={200}
-                                        height={200}
-                                    />
-                                </div>
-                                <div className={styles.topContainer}>
-                                    <h3 className={styles.projectTitle}>{project.name}</h3>
-                                    <p className={styles.description}>{project.description}</p>
-                                </div>
+                                    <div className={styles.imageContainer}>
+                                        <Image
+                                            className={styles.image}
+                                            src={project.image}
+                                            alt={project.name}
+                                            width={200}
+                                            height={200}
+                                        />
+                                    </div>
+                                    <div className={styles.topContainer}>
+                                        <div className={styles.content}>
+                                            <h3 className={styles.projectTitle}>{project.name}</h3>
+                                            <p className={styles.description}>{project.description}</p>
+                                        </div>
+                                    </div>
 
                                 </Link>
                             </div>
-                    )
+                        )
                     })) :
                     <div className={styles.notFoundCard}>
                         <p>No projects found</p>
